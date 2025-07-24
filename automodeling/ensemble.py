aeafd361d46bc60ec30d7e0768edc11259f9b9c6
Cristobal <ctorres@dataqu.cl>
2025-07-23 22:49:12 -0400

    added AutoRandomForestClassifier

--- automodeling/ensemble.py
+++ automodeling/ensemble.py
@@ -1,5 +1,5 @@
-from sklearn.base import BaseEstimator, RegressorMixin
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
+from sklearn.base import BaseEstimator, RegressorMixin, ClassifierMixin
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, RandomForestClassifier
 
 from automodeling.base import AutoModelBase
 
@@ -22,8 +22,8 @@ class AutoRandomForestRegressor(AutoModelBase, BaseEstimator, RegressorMixin):
             min_impurity_decrease=0.0,
             bootstrap=True,
             oob_score=False,
-            n_jobs=-1,
-            random_state=42,
+            n_jobs=None,
+            random_state=None,
             ccp_alpha=0.0,
             max_samples=None,
             auto_scoring=None,
@@ -90,7 +90,7 @@ class AutoRandomForestRegressor(AutoModelBase, BaseEstimator, RegressorMixin):
         params = {
             'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
             'criterion': trial.suggest_categorical('criterion', ['squared_error', 'absolute_error', 'friedman_mse']),
-            'max_depth': trial.suggest_int('max_depth', 3, 30),
+            'max_depth': trial.suggest_int('max_depth', 3, 30) if trial.suggest_categorical('use_max_depth', [True, False]) else None,
             'min_samples_split': trial.suggest_float('min_samples_split', 0.001, 0.3),
             'min_samples_leaf': trial.suggest_float('min_samples_leaf', 0.001, 0.1),
             'min_weight_fraction_leaf': trial.suggest_float('min_weight_fraction_leaf', 0.0, 0.1),
@@ -204,3 +204,116 @@ class AutoGradientBoostingRegressor(AutoModelBase, BaseEstimator, RegressorMixin
         }
         
         return params
+
+class AutoRandomForestClassifier(AutoModelBase, BaseEstimator, ClassifierMixin):
+    def __init__(
+            self,
+            n_estimators=100,
+            criterion='gini',
+            max_depth=None,
+            min_samples_split=2,
+            min_samples_leaf=1,
+            min_weight_fraction_leaf=0.0,
+            max_features='sqrt',
+            max_leaf_nodes=None,
+            min_impurity_decrease=0.0,
+            bootstrap=True,
+            oob_score=False,
+            n_jobs=None,
+            random_state=None,
+            verbose=0,
+            warm_start=False,
+            class_weight=None,
+            ccp_alpha=0.0,
+            max_samples=None,
+            auto_scoring=None,
+            auto_direction='minimize', 
+            auto_timeout=60, 
+            auto_n_trials=None, 
+            auto_verbose=False,
+            auto_use_scaler=False
+        ):
+        super().__init__(
+            auto_scoring,
+            auto_direction,
+            auto_timeout,
+            auto_n_trials,
+            auto_verbose,
+            auto_use_scaler
+        )
+        self.n_estimators = n_estimators
+        self.criterion = criterion
+        self.max_depth = max_depth
+        self.min_samples_split = min_samples_split
+        self.min_samples_leaf = min_samples_leaf
+        self.min_weight_fraction_leaf = min_weight_fraction_leaf
+        self.max_features = max_features
+        self.max_leaf_nodes = max_leaf_nodes
+        self.min_impurity_decrease = min_impurity_decrease
+        self.bootstrap = bootstrap
+        self.oob_score = oob_score
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+        self.verbose = verbose
+        self.warm_start = warm_start
+        self.class_weight = class_weight
+        self.ccp_alpha = ccp_alpha
+        self.max_samples = max_samples
+    
+    def _get_model_params(self):
+        return {
+            'n_estimators': self.n_estimators,
+            'criterion': self.criterion,
+            'max_depth': self.max_depth,
+            'min_samples_split': self.min_samples_split,
+            'min_samples_leaf': self.min_samples_leaf,
+            'min_weight_fraction_leaf': self.min_weight_fraction_leaf,
+            'max_features': self.max_features,
+            'max_leaf_nodes': self.max_leaf_nodes,
+            'min_impurity_decrease': self.min_impurity_decrease,
+            'bootstrap': self.bootstrap,
+            'oob_score': self.oob_score,
+            'n_jobs': self.n_jobs,
+            'random_state': self.random_state,
+            'verbose': self.verbose,
+            'warm_start': self.warm_start,
+            'class_weight': self.class_weight,
+            'ccp_alpha': self.ccp_alpha,
+            'max_samples': self.max_samples
+        }
+    
+    def _build_model(self, params):
+        return RandomForestClassifier(**params)
+
+    def _get_search_space(self, trial):
+        bootstrap = trial.suggest_categorical('bootstrap', [True, False])
+
+        if bootstrap:
+            oob_score = trial.suggest_categorical('oob_score', [False, True])
+        else:
+            oob_score = False
+
+        params = {
+            'n_estimators': trial.suggest_int('n_estimators', 100, 1000),
+            'criterion': trial.suggest_categorical('criterion', ['gini', 'entropy', 'log_loss']),
+            'max_depth': trial.suggest_int('max_depth', 3, 30) if trial.suggest_categorical('use_max_depth', [True, False]) else None,
+            'min_samples_split': trial.suggest_float('min_samples_split', 0.001, 0.3),
+            'min_samples_leaf': trial.suggest_float('min_samples_leaf', 0.001, 0.1),
+            'min_weight_fraction_leaf': trial.suggest_float('min_weight_fraction_leaf', 0.0, 0.1),
+            'max_features': trial.suggest_categorical('max_features', ['sqrt', 'log2', None, 0.3, 0.7, 1.0]),
+            'max_leaf_nodes': trial.suggest_int('max_leaf_nodes', 10, 100) if trial.suggest_categorical('use_max_leaf_nodes', [True, False]) else None,
+            'min_impurity_decrease': trial.suggest_float('min_impurity_decrease', 0.0, 0.01),
+            'bootstrap': bootstrap,
+            'oob_score': oob_score,
+            'n_jobs': self.n_jobs,
+            'random_state': trial.suggest_categorical('random_state', [42]),
+            'verbose': self.verbose,
+            'warm_start': self.warm_start,
+            'class_weight': trial.suggest_categorical('class_weight', ['balanced', 'balanced_subsample', None]),
+            'ccp_alpha': trial.suggest_float('ccp_alpha', 0.0, 0.01),
+            'max_samples': trial.suggest_float('max_samples', 0.3, 1.0) if bootstrap else None,
+        }
+        
+        return params
+    
+    
